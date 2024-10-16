3b109c30c210de86e121c320d3181b98e3d6b217
Denis Korir <denohrkoryr@gmail.com>
2024-10-16 13:45:06 +0300

    Navbar component created

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,27 @@
+import { Link } from "react-router-dom";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="nav-list">
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/services">Services</Link>
+        </li>
+        <li>
+          <Link to="/stylists">Contact</Link>
+        </li>
+        <li>
+          <Link to="/bookings">Bookings</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
