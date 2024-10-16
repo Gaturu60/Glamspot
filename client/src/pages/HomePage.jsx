367873c00694429c221badd33c6e46d76fc7edb3
Denis Korir <denohrkoryr@gmail.com>
2024-10-16 13:49:07 +0300

    HomePage component created

---
+++ client/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+function HomePage() {
+  return (
+    <div className="home-container">
+      <h1>Welcome to Glamspot!</h1>
+      <p>
+        Discover a wide range of salon services, choose from our talented
+        stylists, and book your next appointment with ease.
+      </p>
+    </div>
+  );
+}
+
+export default HomePage;
