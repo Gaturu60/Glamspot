bb5f143583dfac32cfea8cd8ef6e928d7b547002
Denis Korir <denohrkoryr@gmail.com>
2024-10-16 14:44:28 +0300

    Add login link

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -5,19 +5,19 @@ function Navbar() {
     <nav className="navbar">
       <ul className="nav-list">
         <li>
-          <Link to="/">Home</Link>
+          <Link to="/">Home</Link> {/*Link to Home Page*/}
         </li>
         <li>
-          <Link to="/services">Services</Link>
+          <Link to="/services">Services</Link> {/*Link to Services Page*/}
         </li>
         <li>
-          <Link to="/stylists">Contact</Link>
+          <Link to="/stylists">Stylists</Link> {/*Link to Stylists Page*/}
         </li>
         <li>
-          <Link to="/bookings">Bookings</Link>
+          <Link to="/bookings">Bookings</Link> {/*Link to Bookings Page*/}
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/login">Login</Link> {/*Link to Login Page*/}
         </li>
       </ul>
     </nav>
