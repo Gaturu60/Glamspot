969f651f998824b8a5fa217ace9cbc3754092ea4
Denis Korir <denohrkoryr@gmail.com>
2024-10-21 11:46:37 +0300

    updated with password hashing

--- server/models.py
+++ server/models.py
@@ -1,4 +1,5 @@
 from sqlalchemy_serializer import SerializerMixin  # type: ignore
+from werkzeug.security import generate_password_hash, check_password_hash
 from config import db
 
 # Association table for many-to-many relationship between Stylists and Services
@@ -16,6 +17,13 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
+    password_hash = db.Column(db.String(200), nullable=False) #store hashed password
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # One-to-Many Relationship: A user can have many bookings
     bookings = db.relationship('Booking', back_populates='user')
