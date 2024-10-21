0087902f546babf5250308f59b46dc1e0fc73ade
Denis Korir <denohrkoryr@gmail.com>
2024-10-21 12:09:14 +0300

    Implemented Protected Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./pages/HomePage";
@@ -7,16 +7,16 @@ import ServicesPage from "./pages/ServicesPage";
 import StylistsPage from "./pages/StylistsPage";
 import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
+import { AuthContext, AuthProvider } from "./contexts/AuthContext";
 import backgroundImage from "./assets/background/pexels-david-geib-1265112-3268732.jpg";
 
+function ProtectedRoute({ children }) {
+  const { isAuthenticated } = useContext(AuthContext);
+  return isAuthenticated ? children : <Navigate to="/login" />;
+}
 function App() {
   return (
-    <div
-      className="min-h-screen bg-cover bg-center"
-      style={{
-        backgroundImage: `url(${backgroundImage})`, // Use the imported image here
-      }}
-    >
+    <AuthProvider>
       <Router>
         <Navbar />
         <div className="bg-white bg-opacity-90 min-h-screen">
@@ -24,13 +24,22 @@ function App() {
             <Route path="/" element={<HomePage />} />
             <Route path="/services" element={<ServicesPage />} />
             <Route path="/stylists" element={<StylistsPage />} />
-            <Route path="/bookings" element={<BookingPage />} />
             <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<SignUpPage />} />
+
+            {/* Protect Bookings Page */}
+            <Route
+              path="/bookings"
+              element={
+                <ProtectedRoute>
+                  <BookingPage />
+                </ProtectedRoute>
+              }
+            />
           </Routes>
         </div>
       </Router>
-    </div>
+    </AuthProvider>
   );
 }
 
